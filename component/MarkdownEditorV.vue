2b771b349c3bf935322d14739f42593441d1effc
gantrol.q <501963347@qq.com>
2024-03-10 09:09:00 +0800

    fix: 修复样式问题

---
+++ component/MarkdownEditorV.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :id="props.id"></div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import Vditor from 'vditor';
+import 'vditor/dist/index.css';
+
+const props = defineProps({
+  id: {
+    type: String,
+    require: true,
+  }
+});
+
+
+onMounted(() => {
+  // TODO: options
+  const vd = new Vditor(props.id, {
+    theme: "classic",
+  })
+});
+</script>
--- zh/playground/index.md
+++ zh/playground/index.md
@@ -1,1 +1,10 @@
-# TODO
+---
+layout: page
+---
+
+[//]: # (# TODO build playgournd)
+<VditorComponent id="playground-md-editor"/>
+
+<script setup>
+import VditorComponent from "/component/MarkdownEditorV.vue"
+</script>
