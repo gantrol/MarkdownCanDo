bb7ba7f0a777a06e414d3cf1f3ae5413bd4b579f
gantrol.q <501963347@qq.com>
2024-03-17 23:45:31 +0800

    Add Portuguese language support in i18n utility
    The `utils/i18n.ts` file now includes support for the 'pt' (Portuguese) language prefix in URLs. It detects if the current path starts with 'pt', and if so, returns 'pt_BR'. This expands the application's multilingual capabilities by accommodating Portuguese users.

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -3,6 +3,10 @@ export const getI18nForVditor = () => {
     if (currentPath.startsWith('/zh')) {
         return 'zh_CN';
     }
+    else if (currentPath.startsWith('pt')) {
+        return 'pt_BR'
+
+    }
         // else if (currentPath.startsWith('zh')) {
         //
     // }
